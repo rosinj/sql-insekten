7b0807a9d87f5ecc18d678598f43953b3890acf4
JessicaRosin <Jessica_Rosin@web.de>
2021-09-14 15:33:13 +0200

    TASKS unabhängig von Level, Inhalte und Tipps hinzugefügt und geändert; modal global geändert

--- MainViewSkript.js
+++ MainViewSkript.js
@@ -4,7 +4,8 @@
 var hints=3;
 var bbltxtindex=0;
 var fails=0
-var lvl=1;
+// var lvl=1;
+var task_index=0;
 TASKS=[
    {"text": [{"h2": "",
               "h3":"Hier lernst du von Level zu Level wie Hacker dabei vorgehen. Vorkenntisse wie SQL wären empfehlenswert. In diesem Fall haben wir rechts ein Login-Formular, wie man es so von anderen Websiten kennt.",
@@ -12,7 +13,13 @@ TASKS=[
               {"h2": "",
               "h3":"Unter dem Button 'Hintergrunddetails anzeigen', der bei 3 Fehlversuchen aktiviert wird, kannst du dir die live-erzeugte SQL-Query anschauen oder den Hintergrundcode. <br> Zu Beginn hast du 3 Tipps. Einen Tipp kannst du dann bei dem Button 'Tipp einblenden' anzeigen lassen.",
               "img": "img/bee.png"},
-              {"h2": "Los gehts!",
+              {"h2": "",
+              "h3":" Allgemein versucht man durch geschickten Input in den Eingabefeldern eine syntax-korrekte SQL-Query zu erzeugen, die die Aufgabe löst. Hierbei ist es wichtig rumzuprobieren oder sich die Hindergrunddetails anzuschauen um zu wissen, wie die Query mit den Eingabefeldern verknüpft wird, die dann schließlich in der Datenbank ausgeführt wird.",
+              "img": "img/bee.png"},
+              {"h2": "",
+              "h3":"In den ersten Levels wirst du geschickt Apostrophe und den SQL-Befehl '--', der den Code dahinter auskommentiert, verwenden. ",
+              "img": "img/bee.png"},
+              {"h2": "Genug geredet, los gehts!",
               "h3":"",
               "img": "img/happybee.png"}],
     "challenge" : "Versuche als Erstes dich ganz normal als 'maxmustermann' mit einem Passwort einzuloggen, wie man es normalerweise kennt.",
@@ -25,7 +32,7 @@ TASKS=[
                       "whitelist": [""],
                       "blacklist": [""]}],
     "form":"login",
-    "hints"    : "hints deactivated",
+    "hints"    : ["hints deactivated"],
     "behindscene" : "",
     "lvl" : 1},
    {"text": [{"h2": "ok, jetzt geht's aber wirklich los!",
@@ -41,9 +48,9 @@ TASKS=[
                       "whitelist": ["alexamusterfrau"],
                       "blacklist": ["maxmustermann"]}],
     "form":"login",
-    "hints"    : ["Tipp 1: '--' wird in SQL zum auskommentieren verwendet. Dies kannst du hier benutzen um den restlichen Code auszukommentieren bzw. um eine gültige  SQL Query zu erzeugen.","Tipps2"],
+    "hints"    : ["Wie du es in den Hintergrunddetails siehst, ist bevor der Input 'uname' kommt ein Apostroph zu finden. D.h. nachdem du den Benutzernamen in das Eingabefeld eingegeben hast brauchen wir noch ein Apostroph um den String zu beenden. Danach interessiert uns die restliche Query nicht, daher kommentieren wir sie aus."],
     "behindscene" : "",
-    "lvl" : 2},
+    "lvl" : 1},
     {"text" : [{"h2": "Kommen wir zur nächsten Herausforderung",
     "h3":"",
     "img": "img/bee.png"}],
@@ -57,9 +64,9 @@ TASKS=[
                      "whitelist": [""],
                      "blacklist": [""]}],
    "form":"login",
-   "hints"    : ["Tipp1","Tipp2"],
+   "hints"    : ["Nehme die Lösung von Level 1 und quetsche dazwischen ein DROP-TABLE-Befehl. Mit Semikolon trennst du verschiedene Queries voneinander."],
    "behindscene" : "",
-   "lvl": 3},
+   "lvl": 2},
     {"text" :[{"h2": "Kommen wir zur nächsten Herausforderung",
     "h3":"",
     "img": "img/bee.png"},
@@ -76,9 +83,9 @@ TASKS=[
                      "whitelist": [""],
                      "blacklist": [""]}],
    "form":"search",
-   "hints"    : ["Tipp3","Tipp2"],
+   "hints"    : ["hints deactivated"],
    "behindscene" : "",
-   "lvl": 4},
+   "lvl": 3},
    {"text" :[{"h2": "Kommen wir zur nächsten Herausforderung",
       "h3":"",
       "img": "img/bee.png"}],
@@ -92,13 +99,14 @@ TASKS=[
                      "whitelist": [""],
                      "blacklist": [""]}],
    "form":"search",
-   "hints"    : ["Tipp3","Tipp2"],
+   "hints"    : ["Du machst das gleiche wie beim Level davor nur dass du eine andere Eingabe hast und ein anderer Tabellenname."],
    "behindscene" : "",
-   "lvl": 5},
-    {"text" : [{"h2": "Kommen wir zur nächsten Herausforderung",
+   "lvl": 3},
+    {"text" : [{"h2": "Nun sind wir wieder zurück beim Login-Formular! Schauen wir uns mal die nächste Herausforderung an.",
                 "h3":"",
-                "img": "img/bee.png"}],
-    "challenge": "Nun kennst du keinen Nutzernamen. Versuche Informationen über einen oder mehreren Nutzer/n herauszufinden.",
+                "img": "img/happybee.png"},
+               ],
+    "challenge": "Nun kennst du keinen Nutzernamen. Versuche dich trotzdem mit einem User einzuloggen.",
     "validation"  : [{"validationquery": [""],
                       "validationerror":"",
                       "resultlength":1,
@@ -108,9 +116,9 @@ TASKS=[
                       "whitelist": [""],
                       "blacklist": ["maxmustermann","alexamusterfrau"]}],
     "form":"login",
-    "hints"    : ["Tipp1","Tipp2"],
+    "hints"    : ["Wir müssen die Query so geschickt erzeugen, dass sie EINE Zeile ausgibt. Wenn wir die erste WHERE Bedingung leer lassen und eine zweite mit OR hinzufügen die immer stimmt, können wir mit LIMIT beschließen wie viele Zeilen wir ausgeben wollen."],
     "behindscene" : "",
-    "lvl" : 6},
+    "lvl" : 4},
 ];
 var db= createdb();
 createTableUsers(db);
@@ -118,14 +126,14 @@ createTableShoes(db);
 //////////////  HINTS
 
 function show_hints(){
-    var hinttxt=TASKS[lvl-1].hints;
+    var hinttxt=TASKS[task_index].hints;
     document.getElementsByClassName("btnboxtxt")[0].innerHTML=hinttxt[0];
     var e = document.getElementById('btnboxre');
     if(e.style.display == 'block'){
       e.style.display = 'none';
-   // }else if(hinttext[0]=="hints deactivated"){
-
-
+   }else if(TASKS[task_index].hints[0]=="hints deactivated"){
+      document.querySelector("#modal > div >h5 ").innerHTML = 'In dieser Herausforderung gibt es keine Tipps.';
+      document.getElementById("modal").style.display='block';
    }else{
       if(hints>0){
          e.style.display = 'block';
@@ -147,7 +155,8 @@ function show_hints(){
         document.getElementById('light3').src="img/lightoff.png"
        }
        else if(hints<0){
-         document.getElementById("tipp_modal").style.display='block';
+         document.querySelector("#modal > div >h5 ").innerHTML = 'Deine Tipps sind aufgebraucht.';
+         document.getElementById("modal").style.display='block';
        }
    }
 
@@ -167,11 +176,13 @@ function show_behindscene(){
       }
    }
    else{
-      document.getElementById("behindscene_modal").style.display='block';
+      document.querySelector("#modal > div >h5 ").innerHTML ='Der Button wird erst nach 3 Fehlversuchen aktiviert.';
+      document.getElementById("modal").style.display='block';
+
    }
 }
 function update_codetxt(){
-   var form=TASKS[lvl-1].form;
+   var form=TASKS[task_index].form;
    switch (form){
       case "login":
          var uname=document.getElementById("username").value;
@@ -191,7 +202,7 @@ function update_codetxt(){
 }
 function change_codemode(){
    var checked= document.getElementById("switcher").checked;
-   var form=TASKS[lvl-1].form;
+   var form=TASKS[task_index].form;
    if(checked){
       document.getElementById("codetxt").style.display="block";
       document.getElementById("behindcodesearch").style.display="none";
@@ -217,18 +228,18 @@ function change_codemode(){
 function speakbubble_next(){
 
    change_form();
-   if(bbltxtindex >= TASKS[lvl-1].text.length){
+   if(bbltxtindex >= TASKS[task_index].text.length){
       show_lvl();
       bbltxtindex=0;
    }else{
-      document.getElementById("speakbubble_h2").innerHTML = TASKS[lvl-1].text[bbltxtindex].h2;
-      document.getElementById("speakbubble_h3").innerHTML = TASKS[lvl-1].text[bbltxtindex].h3;
-      document.getElementById("insect").src = TASKS[lvl-1].text[bbltxtindex].img;
+      document.getElementById("speakbubble_h2").innerHTML = TASKS[task_index].text[bbltxtindex].h2;
+      document.getElementById("speakbubble_h3").innerHTML = TASKS[task_index].text[bbltxtindex].h3;
+      document.getElementById("insect").src = TASKS[task_index].text[bbltxtindex].img;
       bbltxtindex=bbltxtindex+1;
    }
 }
 function show_lvl(){
-   document.getElementById("speakbubble_h2").innerHTML = TASKS[lvl-1].challenge;
+   document.getElementById("speakbubble_h2").innerHTML = TASKS[task_index].challenge;
    document.getElementById("speakbubble_h3").innerHTML = "";
    document.getElementById("next_btn").style.display = 'none';
 }
@@ -242,7 +253,7 @@ function try_login(){
 }
 
 function change_form(){
-   if(TASKS[lvl-1].form == "login"){
+   if(TASKS[task_index].form == "login"){
       document.getElementById('loginlabel').innerHTML= "Bitte einloggen";
       document.getElementById("username").style.display = 'block';
       document.getElementById("password").style.display = 'block';
@@ -252,7 +263,7 @@ function change_form(){
       document.getElementById("suchergebnisse").style.display = 'none';
       document.getElementById("logout_btn").style.display = 'none';
       document.getElementsByClassName("form-signin")[0].style.maxWidth="350px";
-   }else if(TASKS[lvl-1].form == "search"){
+   }else if(TASKS[task_index].form == "search"){
       document.getElementById('loginlabel').innerHTML= "Produktsuche";
       document.getElementById("username").style.display = 'none';
       document.getElementById("password").style.display = 'none';
@@ -271,7 +282,7 @@ function validation(){
    var lenminus=0;
    var correctanswer = new Array();
    var querysucessful = new Array();
-   var form=TASKS[lvl-1].form;
+   var form=TASKS[task_index].form;
    var query=genarate_query(form);
 
    queries=query.split(";");
@@ -291,18 +302,18 @@ function validation(){
 
                   console.log("iam in the if 2 " + queries[j] + j);
                   console.log(results.rows);
-                  console.log("vorgegeben " + TASKS[lvl-1].validation[0].resultlength +" result length: "+ results.rows.length);
-                  if (results.rows.length == TASKS[lvl-1].validation[0].resultlength) {
+                  console.log("vorgegeben " + TASKS[task_index].validation[0].resultlength +" result length: "+ results.rows.length);
+                  if (results.rows.length == TASKS[task_index].validation[0].resultlength) {
                      querysucessful[j]="true";
                      if(is_query_vaild(form)){
-                        correctanswer[j] = TASKS[lvl-1].validation[0].correctanswer[0];
+                        correctanswer[j] = TASKS[task_index].validation[0].correctanswer[0];
                      }else{
                         correctanswer[j]="false";
                      }
                      console.log("iam in the true section " + queries[j] + correctanswer[j] + j);
                   } else {
                      querysucessful[j]="false";
-                     correctanswer[j] = TASKS[lvl-1].validation[0].correctanswer[1];
+                     correctanswer[j] = TASKS[task_index].validation[0].correctanswer[1];
                      console.log("iam in the false section" + queries[j] + correctanswer[j] + j);
                   }
                   console.log("iam after if 2" + queries[j] + correctanswer[j] + j);
@@ -310,10 +321,10 @@ function validation(){
                   resolve(correctanswer,ergebnis,querysucessful);
 
                   },function(transaction,error){
-                     if(TASKS[lvl-1].validation[0].validationquery[0] !="" && error.message==TASKS[lvl-1].validation[0].validationerror){
-                        correctanswer[j] = TASKS[lvl-1].validation[0].correctanswer[2];
+                     if(TASKS[task_index].validation[0].validationquery[0] !="" && error.message==TASKS[task_index].validation[0].validationerror){
+                        correctanswer[j] = TASKS[task_index].validation[0].correctanswer[2];
                      }else{
-                     correctanswer[j] = TASKS[lvl-1].validation[0].correctanswer[3];
+                     correctanswer[j] = TASKS[task_index].validation[0].correctanswer[3];
                      console.log("iam in the error section" + queries[j] + correctanswer[j] + j);
                      console.log('Error:'+error.message+' (Code '+error.code+')');}
                      querysucessful[j]="error";
@@ -369,15 +380,15 @@ function genarate_query(form){
       default:
          console.log("Error:.");
    }
-   if(TASKS[lvl-1].validation[0].validationquery[0] !="" && query!="notvalid"){
-      query=query + ";" + TASKS[lvl-1].validation[0].validationquery[0];
+   if(TASKS[task_index].validation[0].validationquery[0] !="" && query!="notvalid"){
+      query=query + ";" + TASKS[task_index].validation[0].validationquery[0];
    }
    return query;
  
 }
 function is_query_vaild(form){
-   var blacklistarray=TASKS[lvl-1].validation[0].blacklist;
-   var whitelistarray=TASKS[lvl-1].validation[0].whitelist;
+   var blacklistarray=TASKS[task_index].validation[0].blacklist;
+   var whitelistarray=TASKS[task_index].validation[0].whitelist;
    var valid=true;
    switch (form){
       case "login":
@@ -498,26 +509,27 @@ function answer(correctanswer){
    var answer;
       if (correctanswer.includes("error")){
          fails=fails+1;
-         document.getElementById("speakbubble_h2").innerHTML=TASKS[lvl-1].validation[0].speakbblanswer[1];
+         document.getElementById("speakbubble_h2").innerHTML=TASKS[task_index].validation[0].speakbblanswer[1];
          document.getElementById("speakbubble_h3").innerHTML="";
-         document.getElementById("insect").src = TASKS[lvl-1].validation[0].imganswer[1];
+         document.getElementById("insect").src = TASKS[task_index].validation[0].imganswer[1];
          answer=false;
       }else{
          if(correctanswer.includes("true")){
-            document.getElementById("speakbubble_h2").innerHTML=TASKS[lvl-1].validation[0].speakbblanswer[0];
+            document.getElementById("speakbubble_h2").innerHTML=TASKS[task_index].validation[0].speakbblanswer[0];
             document.getElementById("speakbubble_h3").innerHTML="";
             document.getElementById("next_btn").style.display = 'block';
-            document.getElementById("insect").src = TASKS[lvl-1].validation[0].imganswer[0];
+            document.getElementById("insect").src = TASKS[task_index].validation[0].imganswer[0];
             fails=0;
-            lvl=lvl+1;
-            document.getElementById("lvl").innerHTML="Level: " + lvl;
+            task_index=task_index+1;
+            // lvl=lvl+1;
+            document.getElementById("lvl").innerHTML="Level: " + TASKS[task_index].lvl;
             document.getElementById('btnboxli').style.display = 'none';
             answer=true;
          }else{
             fails=fails+1;
-            document.getElementById("speakbubble_h2").innerHTML=TASKS[lvl-1].validation[0].speakbblanswer[1];
+            document.getElementById("speakbubble_h2").innerHTML=TASKS[task_index].validation[0].speakbblanswer[1];
             document.getElementById("speakbubble_h3").innerHTML="";
-            document.getElementById("insect").src = TASKS[lvl-1].validation[0].imganswer[1];
+            document.getElementById("insect").src = TASKS[task_index].validation[0].imganswer[1];
             answer= false;
          }
       }
@@ -616,20 +628,20 @@ function createdb(){
 }
 //////////////// INFO MODAL 
 function show_info(){
-   e=document.getElementById("info_modal");
+   e=document.getElementById("modal");
    if(e.style.display == 'block'){
       e.style.display = 'none';
    }
    else{
-      if(hints>0){
-         e.style.display = 'block';
-      }
+      document.querySelector("#modal > div >h5 ").innerHTML = "<a href='https://icons8.com/icon/ZiRwjHmdrgtj/info'>Info icon by Icons8</a><br><a target='_blank' href='https://icons8.com/icon/19209/light'>Light</a> icon by <a target='_blank' href='https://icons8.com'>Icons8</a> <br><a href='https://de.freepik.com/vektoren/blume'>Blume Vektor erstellt von terdpongvector - de.freepik.com</a>";
+      e.style.display = 'block';
+      
    }
 }
 
 function close_modal(){
-   e=document.getElementById("info_modal");
-   document.getElementById("behindscene_modal").style.display='none';
-   document.getElementById("tipp_modal").style.display='none';
-   e.style.display = 'none';
+   document.getElementById("modal").style.display='none';
+   // document.getElementById("behindscene_modal").style.display='none';
+   // document.getElementById("tipp_modal").style.display='none';
+   // e.style.display = 'none';
 }
