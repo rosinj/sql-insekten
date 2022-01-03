6b47dc4f5a0b714066673c24d35ca62a58bde640
JessicaRosin <Jessica_Rosin@web.de>
2022-01-03 12:38:49 +0100

    modified menu

--- MainViewSkript.js
+++ MainViewSkript.js
@@ -1276,6 +1276,14 @@ function show_menu(){
    }
 
 }
+function show_lan(){
+   e=document.evaluate("/html/body/div[1]/nav/ul/li/ul/li/ul", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+   if(e.style.display == 'block'){
+      e.style.display = 'none';
+   }else{
+      e.style.display = 'block';
+   }
+}
 
 function close_modal(){
    document.getElementById("modal").style.display='none';
